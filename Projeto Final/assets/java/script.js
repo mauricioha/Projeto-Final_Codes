bf52b6f195fe979a956a77bca2cc68133c5aa56b
mauricioha <38228116+mauricioha@users.noreply.github.com>
2018-11-25 21:02:27 -0200

    Maurício
    Acrescentei algumas pastas para melhorar a organização e adicionei o código de fullscreen.

---
+++ Projeto Final/assets/java/script.js
@@ -0,0 +1,21 @@
+// baseado no código script da pasta js de:
+// https://github.com/hashiprobr/example-fullscreen
+document.addEventListener('DOMContentLoaded', function() {
+  let iframe = document.querySelector('iframe')
+  let button = document.querySelector('button')
+
+  button.addEventListener('click', function() {
+    if(iframe.requestFullscreen) {
+      iframe.requestFullscreen()
+    }
+    else if(iframe.msRequestFullscreen) {
+      iframe.msRequestFullscreen()
+    }
+    else if(iframe.mozRequestFullScreen) {
+      iframe.mozRequestFullScreen()
+    }
+    else if(iframe.webkitRequestFullscreen) {
+      iframe.webkitRequestFullscreen()
+    }
+  })
+})
